1f1bbd7e6fe02472aaaae8d857fe23d2972ccb46
ruizdiaz <pato.ruiz.diaz@hotmail.com>
2022-07-06 13:43:49 -0300

    cambios en la estructura del main.js

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 //reservar turno 
+let turnos = parseInt(prompt(`Ingrese cuantos turnos quiere`));
+while (turnos > 0) {
+    alert(`Pediatras disponibles: \n fernandez \n virgile \n garcia`);//me pone la ubicacion del turno
 
-for (let i = 1; i<=10; i++) {
     let pediatra = prompt("Ingrese nombre del Pediatra");//elijo pediatra
     let ingresarNombre = prompt("Ingresar nombre");// pongo mi nombre
-    alert("turno n° "+i+" Nombre: "+ingresarNombre);//me pone la ubicacion del turno
-}
+  
     
     
     //primero elige pediatra
     switch(pediatra) {
         case "fernandez":
         alert ("su turno fue agendado con fernandez");
+        turnos--;
         break;
         case "virgile":
-        alert ("su turno fue agendado con virgile")
+        alert ("su turno fue agendado con virgile");
+        turnos--;
         break;
         case "garcia":
         alert ("su turno fue agendado con garcia");
+        turnos--;
         break;
         default:
             alert("No se encuentra" + pediatra);
         break;
     }
+}
     
 
 
