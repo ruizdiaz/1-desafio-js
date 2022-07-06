d342e9b70c7412c96c3faaac7b5f2ee97f600380
ruizdiaz <pato.ruiz.diaz@hotmail.com>
2022-07-05 21:07:14 -0300

    agregue un for al main.js

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-//reservar turno es por orden de llegada 
+//reservar turno 
 
-    let pediatra = prompt("Ingrese nombre del Pediatra");
-let nombre = prompt("Ingrese su nombre");
+for (let i = 1; i<=10; i++) {
+    let pediatra = prompt("Ingrese nombre del Pediatra");//elijo pediatra
+    let ingresarNombre = prompt("Ingresar nombre");// pongo mi nombre
+    alert("turno n° "+i+" Nombre: "+ingresarNombre);//me pone la ubicacion del turno
+    break;
+    
+    
+    //primero elige pediatra
+    switch(pediatra) {
+        case "fernandez":
+        alert ("su turno fue agendado con fernandez");
+        break;
+        case "virgile":
+        alert ("su turno fue agendado con virgile")
+        break;
+        case "garcia":
+        alert ("su turno fue agendado con garcia");
+        break;
+        default:
+            alert("No se encuentra" + pediatra);
+        break;
+    }
+    }
 
-
-//primero eligio pediatra
-switch(pediatra) {
-    case "fernandez":
-    alert ("su turno fue agendado con fernandez");
-    break;
-    case "virgile":
-    alert ("su turno fue agendado con virgile")
-    break;
-    case "garcia":
-    alert ("su turno fue agendado con garcia");
-    break;
-    default:
-        alert("No se encuentra" + pediatra);
-    break;
-}
 
 
 
