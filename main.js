c80a6b16960b46745d9f0f3404ccc33136872a0e
ruizdiaz <pato.ruiz.diaz@hotmail.com>
2022-07-18 22:33:04 -0300

    sume arrays al main de js

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 //reservar turno 
-let turnos = parseInt(prompt(`Ingrese cuantos turnos quiere`));//Defino la cantidad de turnos
-while (turnos > 0) {
-    alert(`Pediatras disponibles: \n fernandez \n virgile \n garcia`);//me dice los pediatras disponibles
-
-    let pediatra = prompt("Ingrese nombre del Pediatra");//elijo pediatra
-    let ingresarNombre = prompt("Ingresar nombre");// pongo mi nombre
-  
-    
-    
-    // lista de pediatra que se encuentran disponibles
-    switch(pediatra) {
-        case "fernandez":
-        alert ("su turno fue agendado con fernandez");
-        turnos--;
-        break;
-        case "virgile":
-        alert ("su turno fue agendado con virgile");
-        turnos--;
-        break;
-        case "garcia":
-        alert ("su turno fue agendado con garcia");
-        turnos--;
-        break;
-        default:
-            alert(`No se encuentra ${ pediatra}`);
-        break;
-    }
+let lista = [];
+function agregarTarea (reserva){
+    lista.push(reserva);
+    console.log(lista);
 }
-    
+function sacarTarea(){
+    lista.shift()
+    console.log(lista);
+}
+let toDo = parseInt(prompt("Bienvenidos a Sanitos! \n 1 - Reservar Turno \n 2 - Eliminar Turno \n 3 - esc"));
+while (toDo != 3) {
+    if (toDo === 1) {
+      let newReserva = prompt("Ingresar su nombre");// pongo mi nombre
+      agregarTarea(newReserva);
+      toDo = alert("Su turno fue agendado");
+      toDo = parseInt(prompt("Bienvenidos a Sanitos! \n 1 - Quiere reservar otro turno? \n 2 - Eliminar Turno \n 3 - esc"));
+    } else if (toDo === 2) {
+        let newReserva = prompt("Ingrese su nombre como reservo")
+    sacarTarea(newReserva);
+    toDo = alert("su turno fue cancelado");
+    toDo = parseInt(prompt("Bienvenidos a Sanitos! \n 1 - Reservar Turno \n 2 - Eliminar Turno \n 3 - esc"));
+} else {
+    alert("Dato erroneo, intente nuevamente"),
+    toDo = parseInt(prompt("Bienvenidos a Sanitos! \n 1 - Reservar Turno \n 2 - Eliminar Turno \n 3 - esc"));
+}
+
+}
+alert("Gracias por su visita, vuelva pronto");
+
+   
+
+
 
 
 
